45b54c287b33720b0d6f6355446d5de09557f023
0xkongamoto <hieu.q@autonomous.nyc>
2023-02-23 15:55:05 +0700

    add delegate approval

--- contracts/interface/IDelegateApproval.sol
+++ contracts/interface/IDelegateApproval.sol
@@ -23,13 +23,7 @@ interface IDelegateApproval {
 
     /// @return action The value of action `_CLEARINGHOUSE_OPENPOSITION`
     function getClearingHouseOpenPositionAction() external pure returns (uint8);
-
-    /// @return action The value of action `_CLEARINGHOUSE_ADDLIQUIDITY`
-    function getClearingHouseAddLiquidityAction() external pure returns (uint8);
-
-    /// @return action The value of action `_CLEARINGHOUSE_REMOVELIQUIDITY`
-    function getClearingHouseRemoveLiquidityAction() external pure returns (uint8);
-
+    
     /// @param trader The address of trader
     /// @param delegate The address of delegate
     /// @return actions The approved actions
@@ -39,24 +33,10 @@ interface IDelegateApproval {
     /// @param delegate The address of delegate
     /// @param actions The actions to be checked
     /// @return true if delegate is allowed to perform **each** actions for trader, otherwise false
-    function hasApprovalFor(
-        address trader,
-        address delegate,
-        uint8 actions
-    ) external view returns (bool);
+    function hasApprovalFor(address trader, address delegate, uint8 actions) external view returns (bool);
 
     /// @param trader The address of trader
     /// @param delegate The address of delegate
     /// @return true if delegate can open position for trader, otherwise false
     function canOpenPositionFor(address trader, address delegate) external view returns (bool);
-
-    /// @param trader The address of trader
-    /// @param delegate The address of delegate
-    /// @return true if delegate can add liquidity for trader, otherwise false
-    function canAddLiquidityFor(address trader, address delegate) external view returns (bool);
-
-    /// @param trader The address of trader
-    /// @param delegate The address of delegate
-    /// @return true if delegate can remove liquidity for trader, otherwise false
-    function canRemoveLiquidityFor(address trader, address delegate) external view returns (bool);
 }
